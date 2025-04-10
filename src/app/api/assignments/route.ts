fceed9f571b0364e6449e3c110a56b0ba4339faa
bayoh <bayohi90@gmail.com>
2025-04-10 04:26:22 +0000

    import fix

--- src/app/api/assignments/route.ts
+++ src/app/api/assignments/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server'
-import  prisma  from '@/lib/prisma'
+import  {prisma}  from '@/lib/prisma'
 
 // Assign attendees to rooms
 export async function POST(request: Request) {
